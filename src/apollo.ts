1239d5d6dc0daade2e302286b4e9c81174ef4681
GW <kowonp@gmail.com>
2022-04-21 22:51:57 +0900

    feat(apollo.ts): set authentication

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,8 +1,44 @@
-import { ApolloClient, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar, ApolloLink, NormalizedCacheObject, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+export const TOKEN: string = "TOKEN";
+const token: string | null = localStorage.getItem(TOKEN);
+
+export const isLoggedInVar = makeVar<boolean>(Boolean(token));
+export const tokenVar = makeVar<string | null>(token);
+
+const httpLink: ApolloLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const authLink: ApolloLink = setContext((_: any, { headers }) => {
+  return {
+    headers: { ...headers, token: tokenVar() || "" },
+  };
+});
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              console.log("isLoggedIn read");
+              return false;
+            },
+          },
+          token: {
+            read() {
+              console.log("token read");
+              return tokenVar();
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default client;
